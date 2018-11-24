43ae359d806f0f42cbc8114577b11559907450ca
TGRaydas <pgrand@miuandes.cl>
2018-11-24 18:38:38 -0300

    updates

--- Server.py
+++ Server.py
@@ -1,15 +1,25 @@
-import socket 
-import os             
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)        
+import socket
+import os
+import threading
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = socket.gethostname()
-port = 12345 
-s.bind(("127.0.0.1", port)) 
+port = 12345
+s.bind(("127.0.0.1", port))
 
-file_folder = '/home/raimundo/Desktop/estructura/'
-	
+
+file_folder = os.path.dirname(os.path.realpath(__file__))
+
+# Nombre Archivo, clienteIP, port, ruta
+data_file = open("datafile.txt", "r")
+
+data = []
+
+for linea in data_file:
+	data.append(linea.split(","))
+
+
 def download(name):
-	f = open(file_folder+ name, 'rb')
+	f = open(file_folder + name, 'rb')
 	line = f.read(1024)
 	while line:
 		c.send(line)
@@ -19,29 +29,35 @@ def download(name):
 	c.send("File downloaded".encode())
 
 
+def connection(c, addr, directory):
+	print('Got connection from ' + addr)
+	c.send("Connected to server\nfiles to download: ".encode())
+	c.send(str(directory).encode())
+	while name != "OK":
+		name=c.recv(1024).decode()
+		for i in data:
+			if name == i[0]:
+				c.send("OK".encode())
+				name="OK"
+				c.send(str(i.encode()))
+			if name in i[0]:
+				search.append(i[0])
+				continue
+		c.send(str(search).encode())
+		search=[]
+
+
+
+
+
 s.listen(5)
-search = [] 
-name = ""
+search=[]
+name=""
 while True:
-    directory =os.listdir(file_folder)
-    print()
-    c, addr = s.accept()    
-    print( 'Got connection from', addr)
-    c.send("Connected to server\nfiles to download: ".encode())
-    c.send(str(directory).encode())
-    while name != "OK":	
-    	name = c.recv(1024).decode()
-    	for i in directory:
-	   		if name ==i:
-	   			c.send("OK".encode())
-	   			name = "OK"
-	   			download(i)
+	directory=os.listdir(file_folder)
+	c, addr=s.accept()
+	connection(c, addr)
+	one = threading.Thread(target=connection)
+	one.start()
 
-	   			break
-	   		if name in i:
-	   			search.append(i)
-	   			continue
-    	c.send(str(search).encode())
-    	search = [] 
-
-c.close() 
+c.close()
--- client.py
+++ client.py
@@ -1,56 +1,82 @@
-import socket  
-import os 
+import socket
+import os
+import threading
 
-
-
-def clear(): 
-	os.system('cls' if os.name == 'nt' else 'clear')     
-
-def Menu(s):
-	print(s.recv(4096))
-
-	while True:
-		name=input("name of File: ")  
-		s.send(name.encode())
-		files = s.recv(1024).decode() 
-		if files == "OK":
-			#implente TCP
-			print("file found")
-			recive(name)
-
-
-		else:
-			print(files)
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 
 def recive(name):
-	f = open(name, 'wb')
-	while(True):
-		l = s.recv(1024)
-		if "Sent" in l.decode():
-			l = s.recv(1024).decode()
-			print(l)
-			break
-		f.write(l)
-		if l == None:
-			break
+    f = open(name, 'wb')
+    while(True):
+        l = s.recv(1024)
+        if "Sent" in l.decode():
+            l = s.recv(1024).decode()
+            print(l)
+            break
+        f.write(l)
+        if l == None:
+            break
 
 
-		
+def download(name):
+    f = open(file_folder + name, 'rb')
+    line = f.read(1024)
+    while line:
+        c.send(line)
+        line = f.read(1024)
+    f.close()
+    c.send("Sent".encode())
+    c.send("File downloaded".encode())
 
-	
+
+def Menu(s):
+    print(s.recv(4096))
+    while True:
+        name = input("name of File: ")
+        s.send(name.encode())
+        files = s.recv(1024).decode()
+        if files == "OK":
+            # implente TCP4
+            name = s.recv(1024).decode()
+            print(name)
+            print("file found")
+        else:
+            print(files)
 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         
-host = socket.gethostname() 
-port = 12345 
-try:
-    s.connect(("127.0.0.1", port))
+def p2p():
+    s_p2p = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host_p2p = socket.gethostname()
+    port_p2p = 1234
+    s_p2p.bind(("127.0.0.1", port_p2p))
+    s_p2p.listen(5)
+    while True:
+        c, addr = s_p2p.accept()
+        name = c.recv(1024).decode()
+        download(name)
+        c.close()
 
-except Exception as e:
-    print("Error!")
-    exit(1)
-  
-print( s.recv(1024).decode())
-Menu(s)
-s.close                 
+
+def client():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = socket.gethostname()
+    port = 12345
+
+    try:
+        s.connect(("127.0.0.1", port))
+
+    except Exception as e:
+        print("Error!")
+        exit(1)
+    print(s.recv(1024).decode())
+    Menu(s)
+    s.close
+
+
+
+client_t = threading.Thread(target=client)
+
+p2p_t = threading.Thread(target=p2p)
+p2p_t.start()
+client_t.start()
